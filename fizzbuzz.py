79842196896b53552a0cb79cd1eb7b9f04e5ac69
traveldud <36678955+traveldud@users.noreply.github.com>
2019-04-08 15:28:29 -0400

    Add files via upload

---
+++ fizzbuzz.py
@@ -0,0 +1,27 @@
+
+a=3
+b=5
+c=1
+d=100
+
+
+
+i = 1
+while i <= 100:
+   if i/15 == int(i/15):
+       print('FizzBuzz')
+   elif i/3 == int(i/3):
+       print('Fizz')
+   elif i/5 == int(i/5):
+       print('Buzz')
+   else:
+       print(i)
+   i=i+1
+   
+   
+    
+ #   elif c/5 == id(int):
+  #      print('Buzz')
+   # elif c/15 == id(int):
+    #    print('FizzBuzz')
+  
