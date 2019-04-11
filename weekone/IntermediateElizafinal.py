557e592232844c281314756b85fa1b03132407a2
traveldud <36678955+traveldud@users.noreply.github.com>
2019-04-11 11:58:15 -0400

    Update IntermediateElizafinal.py

--- weekone/IntermediateElizafinal.py
+++ weekone/IntermediateElizafinal.py
@@ -1,3 +1,39 @@
+#enter a concern to the bot Eliza and she will respond
+
+prompt = "\ntell me something and let me show you that I care"
+prompt += "\nEnter 'quit' or 'I'm feeling great: "
+new_list = []
+active = True
+while active:
+	msg = input(prompt)
+	new_msg = msg.lower()
+	if new_msg == "quit" or new_msg == "i'm feeling great":
+		active = False
+	else: 
+		list = new_msg.split()
+		new_list=[]
+		
+		#append the entry to the string as it cycles through clients statement
+		#with appropriate changes
+		for ea in list:
+			if ea == "i":
+				new_list.append("you")
+			elif ea == "me":
+				new_list.append("you")
+			elif ea == "my":
+				new_list.append("your")
+			elif ea == "am":
+				new_list.append("are")
+			else:
+				new_list.append(ea)
+		
+		print(" ".join(new_list), '?')
+
+
+
+"""---------------------------------------------------------
+this is the same as above w/out declarations
+
 prompt = "\ntell me something and let me show you that I care"
 prompt += "\nEnter 'quit' or 'I'm feeling great: "
 new_list = []
@@ -31,3 +67,4 @@ while active:
 
 			
 		print(" ".join(new_list), '?')
+		"""
